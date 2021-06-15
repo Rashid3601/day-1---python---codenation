ee007a37bd53fe2da44187ec67558d7c7a8aaf8e
Rashid3601 <rashidsadi55@gmail.com>
2021-06-15 15:25:11 +0100

    day-1---python---codenation

---
+++ day1.py
@@ -0,0 +1,32 @@
+print("Hello World")
+
+for i in range(10):
+    print(i)
+
+print("Steak and chips")
+#this is a comment
+print("hello".upper())
+import random
+print(random.random()*10)
+import random
+print(random.uniform(1,10))
+import random
+print (random.randint(1,10))
+print ("---------------------")
+print (" |        |         |")
+print (" |        |         |")
+print (" |        |         |")
+print ("---------------------")
+print (" |        |         |")
+print (" |        |         |")
+print (" |        |         |")
+print ("---------------------")
+print("hello".upper()) #this is uppercase
+print("hello".lower()) #this is lowercase
+print("hello".capitalize()) #this is capitalize
+print("hello".count("h")) #this is a count
+print("hello".find("l")) #this is a find
+print("hello".replaced("e")) #this is replaced
+import random
+print(random.randint(3,9))
+
